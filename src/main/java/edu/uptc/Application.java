674f5bcc1107ff5e0178cb2dd8370ad507c7e9c1
SebaZnX <sebastianalvarezp09@gmail.com>
2026-05-04 15:06:06 -0500

    Crud realizado

--- src/main/java/edu/uptc/Application.java
+++ src/main/java/edu/uptc/Application.java
@@ -18,9 +18,8 @@ public class Application {
                 6. Salir.
                 -------------------------------------
                 """;
-        String menuCase1 = """
+        String menuDocumentos = """
                 -------------------------------------
-                ¿Que tipo de documento quiere crear?
                 1. Articulo cientifico
                 2. Libros academicos
                 3. Memorias de conferencia
@@ -33,7 +32,7 @@ public class Application {
             switch (op) {
                 case 1:
                     int opCase1 = 0;
-                    opCase1 = Integer.parseInt(JOptionPane.showInputDialog(menuCase1));
+                    opCase1 = Integer.parseInt(JOptionPane.showInputDialog("¿Que documento desea crear?\n" + menuDocumentos));
 
                     if ((opCase1 < 1) || (opCase1 > 3)) {
                         JOptionPane.showMessageDialog(null, "La opcion no esta disponible, intente de nuevo");
@@ -105,14 +104,110 @@ public class Application {
                     }
                     break;
                 case 2:
-                    JOptionPane.showMessageDialog(null, gestionSistema.toString());
+                    JOptionPane.showMessageDialog(null, "LISTA ARTICULO CIENFIFICO\n" + gestionSistema.mostrarArticulosInvestigacion());
+                    JOptionPane.showMessageDialog(null, "LISTA LIBROS ACADEMICOS\n" + gestionSistema.mostrarLibrosAcademicos());
+                    JOptionPane.showMessageDialog(null, "LISTA MEMORIAS DE CONFERENCIAS\n" + gestionSistema.mostrarMemoriasConferencias());
 
                     break;
                 case 3:
+                    String idBuscarActualizacion = JOptionPane.showInputDialog("Ingrese el id a actualizar");
+                    boolean encontradoIdActualizar = gestionSistema.buscarIdDocumento(idBuscarActualizacion);
+                    if (encontradoIdActualizar) {
+                        int opHashMapModificar = gestionSistema.asignacionParaActualizar(idBuscarActualizacion);
+                        String tituloDocumentoAcademico = null;
+                        String inicialesyApellidosAutor = null;
+                        String anhoPubliacion = null;
+                        boolean modificado = false;
+                        switch (opHashMapModificar) {
+                            case 1:
+                                JOptionPane.showMessageDialog(null, "ACTUALIZACION DE UN ARTICULO CIENTIFICO\n INGRESE LOS SIGUIENTES DATOS");
+                                tituloDocumentoAcademico = JOptionPane.showInputDialog("Ingrese el titulo del articulo cientifico");
+                                inicialesyApellidosAutor = JOptionPane.showInputDialog("Ingrese las iniciales y apellidos del articulo cientifico");
+                                String tituloRevista = JOptionPane.showInputDialog("Ingrese el titulo de la revista abreviado");
+                                String volumen = JOptionPane.showInputDialog("Ingrese el volumen del articulo cientifico");
+                                String numero = JOptionPane.showInputDialog("Ingrese el numero del articulo cientifico");
+                                String paginas = JOptionPane.showInputDialog("Ingrese las paginas del articulo cientifico");
+                                String mes = JOptionPane.showInputDialog("Ingrese el mes del articulo cientifico");
+                                anhoPubliacion = JOptionPane.showInputDialog("Ingrese el año de publicacion del articulo cientifico");
+
+                                modificado = gestionSistema.actualizarInfoArticuloInvestigacion(idBuscarActualizacion, tituloDocumentoAcademico,
+                                        anhoPubliacion, inicialesyApellidosAutor, tituloRevista, volumen, numero,
+                                        paginas, mes);
+                                if (modificado) {
+                                    JOptionPane.showMessageDialog(null, "Articulo cientifico actualizado");
+                                } else {
+                                    JOptionPane.showMessageDialog(null, "Articulo cientifico no modificado");
+                                }
+                                break;
+                            case 2:
+                                JOptionPane.showMessageDialog(null, "ACTUALIZACION DE UN LIBRO ACADEMICO\n INGRESE LOS SIGUIENTES DATOS");
+                                tituloDocumentoAcademico = JOptionPane.showInputDialog("Ingrese el titulo del libro");
+                                inicialesyApellidosAutor = JOptionPane.showInputDialog("Ingrese las iniciales y apellidos del autor del libro");
+                                String edicion = JOptionPane.showInputDialog("Ingrese la edicion del libro");
+                                String lugarPublicacion = JOptionPane.showInputDialog("Ingrese el lugar de la publicacion del libro");
+                                String editorial = JOptionPane.showInputDialog("Ingrese la editorial del libro");
+                                anhoPubliacion = JOptionPane.showInputDialog("Ingrese el año de publicacion del libro");
+
+                                modificado = gestionSistema.actualizarInfoLibrosAcademico(idBuscarActualizacion, tituloDocumentoAcademico, anhoPubliacion,
+                                        inicialesyApellidosAutor, edicion, lugarPublicacion, editorial);
+                                if (modificado) {
+                                    JOptionPane.showMessageDialog(null, "Libro academico actualizado");
+                                } else {
+                                    JOptionPane.showMessageDialog(null, "Libro academico no modificado");
+                                }
+                                break;
+                            case 3:
+                                JOptionPane.showMessageDialog(null, "ACTUALIZACION DE UN MEMORIAS DE CONFERENCIA\n INGRESE LOS SIGUIENTES DATOS");
+                                tituloDocumentoAcademico = JOptionPane.showInputDialog("Ingrese el titulo de la memoria de la conferencia");
+                                inicialesyApellidosAutor = JOptionPane.showInputDialog("Ingrese las iniciales y apellidos de la memoria de la conferencia");
+                                String nombreCompletoConferencia = JOptionPane.showInputDialog("Ingrese el nombre completo de la memoria de la conferencia");
+                                String ciudadConferencia = JOptionPane.showInputDialog("Ingrese la ciudad de la memoria de la conferencia");
+                                String estadoConferencia = JOptionPane.showInputDialog("Ingrese el estado de la memoria de la conferencia");
+                                String paginasConferencia = JOptionPane.showInputDialog("Ingrese las paginas de la memoria de la conferencia");
+                                anhoPubliacion = JOptionPane.showInputDialog("Ingrese el año de la memoria de la conferencia ");
+
+                                modificado = gestionSistema.actualizarMemoriaConferencia(idBuscarActualizacion, tituloDocumentoAcademico,
+                                        anhoPubliacion, inicialesyApellidosAutor, nombreCompletoConferencia,
+                                        ciudadConferencia, estadoConferencia, paginasConferencia);
+                                if (modificado) {
+                                    JOptionPane.showMessageDialog(null, "Memorias de conferencia actualizado");
+                                } else {
+                                    JOptionPane.showMessageDialog(null, "Memorias de conferencia no modificado");
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(null, "No existe es Id");
+                    }
                     break;
                 case 4:
+                    String idEliminar = JOptionPane.showInputDialog("Ingrese el id a eliminar");
+                    boolean existeIdEliminar = gestionSistema.buscarIdDocumento(idEliminar);
+                    if (existeIdEliminar) {
+                        boolean fueEliminado = gestionSistema.eliminarDocumentoAcademico(idEliminar);
+                        if (fueEliminado) {
+                            JOptionPane.showMessageDialog(null, "Documento academico eliminado");
+                        } else {
+                            JOptionPane.showMessageDialog(null, "No fue posible eliminar el documento academico");
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(null, "El id no existe");
+                    }
                     break;
                 case 5:
+                    int opGenerarRefenciaBibliografica = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la refencia bibliografica a generar\n" + menuDocumentos));
+                    switch (opGenerarRefenciaBibliografica) {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        default:
+                            break;
+                    }
                     break;
                 default:
                     break;
--- src/main/java/edu/uptc/GestionSistema.java
+++ src/main/java/edu/uptc/GestionSistema.java
@@ -66,9 +66,9 @@ public class GestionSistema {
     }
 
 
-    public void actualizarInfoArticuloInvestigacion(String idDocumentoAcademico, String tituloDocumento, String anhoPubliacion,
-                                                    String inicialesyApellidosAutor, String tituloRevista,
-                                                    String volumen, String numero, String paginas, String mes) {
+    public boolean actualizarInfoArticuloInvestigacion(String idDocumentoAcademico, String tituloDocumento, String anhoPubliacion,
+                                                       String inicialesyApellidosAutor, String tituloRevista,
+                                                       String volumen, String numero, String paginas, String mes) {
         articulosInvestigacion.get(idDocumentoAcademico).setTituloDocumento(tituloDocumento);
         articulosInvestigacion.get(idDocumentoAcademico).setAnhoPubliacion(anhoPubliacion);
         articulosInvestigacion.get(idDocumentoAcademico).setInicialesyApellidosAutor(inicialesyApellidosAutor);
@@ -78,22 +78,25 @@ public class GestionSistema {
         articulosInvestigacion.get(idDocumentoAcademico).setPaginas(paginas);
         articulosInvestigacion.get(idDocumentoAcademico).setMes(mes);
 
+        return true;
     }
 
-    public void actualizarInfoLibrosAcademico(String idDocumentoAcademico, String tituloDocumento, String anhoPubliacion,
-                                              String inicialesyApellidosAutor, String edicion, String lugarPublicacion,
-                                              String editorial) {
+    public boolean actualizarInfoLibrosAcademico(String idDocumentoAcademico, String tituloDocumento, String anhoPubliacion,
+                                                 String inicialesyApellidosAutor, String edicion, String lugarPublicacion,
+                                                 String editorial) {
         librosAcademicos.get(idDocumentoAcademico).setTituloDocumento(tituloDocumento);
         librosAcademicos.get(idDocumentoAcademico).setAnhoPubliacion(anhoPubliacion);
         librosAcademicos.get(idDocumentoAcademico).setInicialesyApellidosAutor(inicialesyApellidosAutor);
         librosAcademicos.get(idDocumentoAcademico).setEdicion(edicion);
         librosAcademicos.get(idDocumentoAcademico).setLugarPublicacion(lugarPublicacion);
         librosAcademicos.get(idDocumentoAcademico).setEditorial(editorial);
+
+        return true;
     }
 
-    public void actualizarMemoriaConferencia(String idDocumentoAcademico, String tituloDocumento, String anhoPubliacion,
-                                             String inicialesyApellidosAutor, String nombreCompletoConferencia,
-                                             String ciudadConferencia, String estadoConferencia, String paginas) {
+    public boolean actualizarMemoriaConferencia(String idDocumentoAcademico, String tituloDocumento, String anhoPubliacion,
+                                                String inicialesyApellidosAutor, String nombreCompletoConferencia,
+                                                String ciudadConferencia, String estadoConferencia, String paginas) {
         memoriasConferencias.get(idDocumentoAcademico).setTituloDocumento(tituloDocumento);
         memoriasConferencias.get(idDocumentoAcademico).setAnhoPubliacion(anhoPubliacion);
         memoriasConferencias.get(idDocumentoAcademico).setInicialesyApellidosAutor(inicialesyApellidosAutor);
@@ -101,10 +104,12 @@ public class GestionSistema {
         memoriasConferencias.get(idDocumentoAcademico).setCiudadConferencia(ciudadConferencia);
         memoriasConferencias.get(idDocumentoAcademico).setEstadoConferencia(estadoConferencia);
         memoriasConferencias.get(idDocumentoAcademico).setPaginas(paginas);
+
+        return true;
     }
 
 
-    public boolean eliminarArticuloInvestigacionPorId(String idDocumentoAcademico) {
+  /*  public boolean eliminarArticuloInvestigacionPorId(String idDocumentoAcademico) {
         articulosInvestigacion.remove(idDocumentoAcademico);
         return true;
     }
@@ -118,13 +123,39 @@ public class GestionSistema {
         memoriasConferencias.remove(idDocumentoAcademico);
         return true;
     }
+*/
+    // No se pueden eliminar dos objetos ya que todos tiene un id diferente independientemente si estan en diferente hashmap.
+    // Al ser un idGlobal no hay un id que se repita
 
-    @Override
-    public String toString() {
-        return "GestionSistema{" +
-                "articulosInvestigacion=" + articulosInvestigacion +
-                ", librosAcademicos=" + librosAcademicos +
-                ", memoriasConferencias=" + memoriasConferencias +
-                '}';
+    public boolean eliminarDocumentoAcademico(String idDocumento) {
+        articulosInvestigacion.remove(idDocumento);
+        librosAcademicos.remove(idDocumento);
+        memoriasConferencias.remove(idDocumento);
+        return true;
+    }
+
+    public boolean buscarIdDocumento(String idBuscar) {
+        if (articulosInvestigacion.containsKey(idBuscar)) {
+            return true;
+        } else if (librosAcademicos.containsKey(idBuscar)) {
+            return true;
+        } else if (memoriasConferencias.containsKey(idBuscar)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int asignacionParaActualizar(String idAsignar) {
+        if (articulosInvestigacion.containsKey(idAsignar)) {
+            return 1;
+        } else if (librosAcademicos.containsKey(idAsignar)) {
+            return 2;
+        } else if (memoriasConferencias.containsKey(idAsignar)) {
+            return 3;
+        } else {
+            return 0;
+        }
     }
 }
+
